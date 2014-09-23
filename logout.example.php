bf22d13302c641b2e701b21e403ac6d5055370e4
Stefan Koolen <nast3zz@gmail.com>
2014-09-23 17:06:21 +0200

    [Delete] logout.example.php
    Minor commit fail. Sent a file accidentally double.

--- logout.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-	mysql_connect("", "", "") or die(mysql_error());
-	mysql_select_db("") or die(mysql_error());
-	session_start();
-	mysql_query("DELETE FROM `user_session` WHERE `user_session`.`hash` = '". $_COOKIE['user_session'] ."'") or die(mysql_error());
-	$_SESSION['user'] = "";
-	setcookie("user_session", "", time()-3600);
-	header("location: index.php");
-?>
